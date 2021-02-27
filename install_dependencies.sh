2f93d808f0c89a0e17ee181ad5dfc6fb05ef9aca
Darshan Sheth <darshan.sheth@numerator.com>
2021-02-27 17:07:33 +0530

    initial commit#9

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+pwd
+ls
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt-cache madison docker-ce
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
